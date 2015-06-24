1fb3beeb12235fd86dab83e469d62d584c36390b
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-06-24 11:56:42 +0000

    Moved to clean project structure
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Analyzer/ProtoCom/trunk@27670 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.protocom.framework.java.ee/src/org/palladiosimulator/protocom/framework/java/ee/main/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Main classes and entry point for Java EE performance prototypes.
+ * @author Christian Klaussner
+ */
+package org.palladiosimulator.protocom.framework.java.ee.main;
---
+++ org.palladiosimulator.protocom.framework.java.ee/src/org/palladiosimulator/protocom/framework/java/ee/protocol/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Classes for the HTTP-based object registry and communication protocol.
+ * @author Christian Klaussner
+ */
+package org.palladiosimulator.protocom.framework.java.ee.protocol;
