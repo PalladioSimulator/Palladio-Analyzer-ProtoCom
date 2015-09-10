84172672bfdfc52329de4a07745be41524ad87ca
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-09-10 14:29:43 +0000

    Refactored library handling: now looking into the Eclipse platform to determine the current JAR. Still some testing to do; will work on that after the build succeeded once...
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Analyzer/ProtoCom/trunk@29069 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.protocom.framework.java.ee/src/org/palladiosimulator/protocom/framework/java/ee/webcontent/FileProvider.java
+++ org.palladiosimulator.protocom.framework.java.ee/src/org/palladiosimulator/protocom/framework/java/ee/webcontent/FileProvider.java
@@ -1,9 +1,10 @@
 package org.palladiosimulator.protocom.framework.java.ee.webcontent;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
@@ -18,99 +19,158 @@ import org.osgi.framework.Constants;
 
 /**
  * The FileProvider class is used to retrieve WebContent files during transformation.
- * @author Christian Klaussner
+ * 
+ * @author Christian Klaussner, Sebastian Lehrig
  */
 public class FileProvider {
 
-	/**
-	 * Returns a file in the framework as specified in the index.
-	 * @param source the name of the file
-	 * @param path the path of the file
-	 * @return a file in the framework
-	 */
-	private FrameworkFile processIndex(String source, String path) {
-		URL url = getClass().getResource("files/" + source);
-		String absolute = path + '/' + source.substring(source.lastIndexOf('/') + 1);
+    /**
+     * Returns a file in the framework as specified in the index.
+     * 
+     * @param source
+     *            the name of the file
+     * @param path
+     *            the path of the file
+     * @return a file in the framework
+     */
+    private FrameworkFile processIndex(final String source, final String path) {
+        final URL url = getClass().getResource("files/" + source);
+        final String absolute = path + '/' + source.substring(source.lastIndexOf('/') + 1);
 
-		return new FrameworkFile(url, absolute);
-	}
+        return new FrameworkFile(url, absolute);
+    }
 
-	/**
-	 * Gets all framework files specified in the index.
-	 * @return a list of files in the framework
-	 */
-	public List<FrameworkFile> getFrameworkFiles() {
-		List<FrameworkFile> files = new LinkedList<FrameworkFile>();
+    /**
+     * Returns a file in the framework as specified in the index.
+     * 
+     * @param source
+     *            the name of the file
+     * @param path
+     *            the path of the file
+     * @return a file in the framework
+     */
+    private FrameworkFile processPlugin(final String source) {
+        final File file = getPluginJarFile(source);
+        final String absolute = "WEB-INF/lib/" + file.getName();
 
-		InputStream in;
+        try {
+            return new FrameworkFile(file.toURI().toURL(), absolute);
+        } catch (final MalformedURLException e) {
+            throw new RuntimeException("Cannot convert file \"" + file + "\" to URL (malformed)");
+        }
+    }
 
-		try {
-			in = getClass().getResource("index").openStream();
-		} catch (IOException e) {
-			throw new RuntimeException("Index file not found", e);
-		}
+    /**
+     * Gets all framework files specified in the index.
+     * 
+     * @return a list of files in the framework
+     */
+    public List<FrameworkFile> getFrameworkFiles() {
+        final List<FrameworkFile> files = new LinkedList<FrameworkFile>();
+        String line;
 
-		BufferedReader index = new BufferedReader(new InputStreamReader(in));
-		String line;
+        try (final BufferedReader reader = getReader("files.index")) {
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("#") || line.trim().isEmpty()) {
+                    continue;
+                }
 
-		try {
-			while ((line = index.readLine()) != null) {
-				if (line.startsWith("#") || line.trim().isEmpty()) {
-					continue;
-				}
+                final String[] comp = line.split("->");
 
-				String[] comp = line.split("->");
+                if (comp.length != 2) {
+                    throw new RuntimeException("Wrong index file format");
+                } else {
+                    files.add(processIndex(comp[0].trim(), comp[1].trim()));
+                }
+            }
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
 
-				if (comp.length != 2) {
-					throw new RuntimeException("Wrong index file format");
-				} else {
-					files.add(processIndex(comp[0].trim(), comp[1].trim()));
-				}
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+        try (final BufferedReader reader = getReader("libraries.index")) {
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("#") || line.trim().isEmpty()) {
+                    continue;
+                }
 
-		return files;
-	}
+                files.add(processPlugin(line.trim()));
+            }
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
 
-	/**
-	 * Fills the given set with the dependencies of the specified bundle.
-	 * Used only for debugging purposes.
-	 *
-	 * @param bundleName the name of the bundle
-	 * @param result the set used to store the names of the bundle's dependencies
-	 */
-	public void getDependencies(String bundleName, Set<String> result) {
-		Bundle bundle = Platform.getBundle(bundleName);
+        return files;
+    }
 
-		if (bundle == null) {
-			return;
-		}
+    private BufferedReader getReader(final String file) {
+        try {
+            return new BufferedReader(new InputStreamReader(getClass().getResource(file).openStream()));
+        } catch (final IOException e) {
+            throw new RuntimeException("Index file not found", e);
+        }
+    }
 
-		String header = bundle.getHeaders().get(Constants.REQUIRE_BUNDLE);
-		String jar = "<unknown>";
+    /**
+     * @see http://www.eclipsezone.com/eclipse/forums/t49415.html
+     * 
+     * @param pluginID
+     *            Plug-in ID for of the Jar file to be loaded
+     * @return the plug-in's Jar file
+     */
+    private File getPluginJarFile(final String pluginID) {
+        final Bundle plugin = Platform.getBundle(pluginID);
 
-		try {
-			jar = FileLocator.getBundleFile(bundle).getName();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        if (plugin == null) {
+            throw new RuntimeException("Plug-In with ID \"" + pluginID + "\" cannot be resolved");
+        }
 
-		try {
-			ManifestElement[] elements;
-			elements = ManifestElement.parseHeader(Constants.BUNDLE_CLASSPATH, header);
+        try {
+            return FileLocator.getBundleFile(plugin);
+        } catch (final IOException e) {
+            throw new RuntimeException("No access for reading \"" + plugin + "\"");
+        }
+    }
 
-			if (elements != null) {
-				for (ManifestElement element : elements) {
-					String value = element.getValue();
+    /**
+     * Fills the given set with the dependencies of the specified bundle. Used only for debugging
+     * purposes.
+     * 
+     * @see http://www.eclipsezone.com/eclipse/forums/t49415.html
+     * @param pluginID
+     *            the name of the bundle
+     * @param result
+     *            the set used to store the names of the bundle's dependencies
+     */
+    public void getDependencies(final String pluginID, final Set<String> result) {
+        final Bundle plugin = Platform.getBundle(pluginID);
 
-					result.add(value + " (" + jar + ")");
-					getDependencies(value, result);
-				}
-			}
-		} catch (BundleException e) {
-			e.printStackTrace();
-		}
-	}
+        if (plugin == null) {
+            return;
+        }
+
+        final String requires = plugin.getHeaders().get(Constants.REQUIRE_BUNDLE);
+        String jar = "<unknown>";
+
+        try {
+            jar = FileLocator.getBundleFile(plugin).getName();
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            ManifestElement[] elements;
+            elements = ManifestElement.parseHeader(Constants.BUNDLE_CLASSPATH, requires);
+
+            if (elements != null) {
+                for (final ManifestElement element : elements) {
+                    final String value = element.getValue();
+
+                    result.add(value + " (" + jar + ")");
+                    getDependencies(value, result);
+                }
+            }
+        } catch (final BundleException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- org.palladiosimulator.protocom.framework.java.ee/src/org/palladiosimulator/protocom/framework/java/ee/webcontent/FrameworkFile.java
+++ org.palladiosimulator.protocom.framework.java.ee/src/org/palladiosimulator/protocom/framework/java/ee/webcontent/FrameworkFile.java
@@ -4,35 +4,42 @@ import java.net.URL;
 
 /**
  * The FrameworkFile class stores the data required for copying a framework file.
- * @author Christian Klaussner
+ * 
+ * @author Christian Klaussner, Sebastian Lehrig
  */
 public class FrameworkFile {
-	private final URL url;
-	private final String path;
 
-	/**
-	 * Constructs a new FrameworkFile object.
-	 * @param url the source URL
-	 * @param path the destination path
-	 */
-	public FrameworkFile(URL url, String path) {
-		this.url = url;
-		this.path = path;
-	}
+    private final URL url;
+    private final String path;
 
-	/**
-	 * Gets the source URL.
-	 * @return the source URL
-	 */
-	public URL getUrl() {
-		return url;
-	}
+    /**
+     * Constructs a new FrameworkFile object.
+     * 
+     * @param url
+     *            the source URL, e.g., URL("img/favicon.png")
+     * @param path
+     *            the destination path, e.g., "img"
+     */
+    public FrameworkFile(final URL url, final String path) {
+        this.url = url;
+        this.path = path;
+    }
 
-	/**
-	 * Gets the destination path.
-	 * @return the destination path
-	 */
-	public String getPath() {
-		return path;
-	}
+    /**
+     * Gets the source URL.
+     * 
+     * @return the source URL
+     */
+    public URL getUrl() {
+        return this.url;
+    }
+
+    /**
+     * Gets the destination path.
+     * 
+     * @return the destination path
+     */
+    public String getPath() {
+        return this.path;
+    }
 }
